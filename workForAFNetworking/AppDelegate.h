db635dfc30eda168f6b80c9d1a627688036dd52d
harikrishna <harikrishna@neolinx.com.np>
2014-01-06 17:41:40 +0545

    Initialization

---
+++ workForAFNetworking/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  workForAFNetworking
+//
+//  Created by ATI on 1/6/14.
+//  Copyright (c) 2014 ATI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ workForAFNetworking/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  workForAFNetworking
+//
+//  Created by ATI on 1/6/14.
+//  Copyright (c) 2014 ATI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController 
+
+@end
